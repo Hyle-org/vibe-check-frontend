ea13ea153beb909d82e83c0078d075e9031456cd
Lancelot de Ferrière <wraitii@gmail.com>
2024-06-10 11:49:34 +0200

    Face detection with face-api.js

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,56 @@
 <script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue";
+import * as faceApi from "face-api.js";
+import { onMounted, ref } from "vue";
+
+// resize the overlay canvas to the input dimensions
+const canvasOutput = ref<HTMLCanvasElement | null>(null);
+const videoFeed = ref<HTMLVideoElement | null>(null);
+
+const detectionTimer = ref<unknown | null>(null);
+
+onMounted(async () => {
+    await faceApi.nets.tinyFaceDetector.loadFromUri("/models");
+    navigator.mediaDevices
+        .getUserMedia({ video: true, audio: false })
+        .then(async (stream) => {
+            videoFeed.value!.srcObject = stream;
+            videoFeed.value!.play();
+            detectionTimer.value = setInterval(async () => {
+                const displaySize = { width: videoFeed.value!.clientWidth, height: videoFeed.value!.clientHeight }
+                faceApi.matchDimensions(canvasOutput.value!, displaySize)
+                const detections = await faceApi.detectAllFaces(videoFeed.value!, new faceApi.TinyFaceDetectorOptions())
+                const resizedDetections = faceApi.resizeResults(detections, displaySize)
+                faceApi.draw.drawDetections(canvasOutput.value!, resizedDetections)
+            }, 1000);
+        })
+        .catch((err) => {
+            console.error(`An error occurred: ${err}`);
+        });
+});
 </script>
 
 <template>
-    <div>
-        <a href="https://vitejs.dev" target="_blank">
-            <img src="/vite.svg" class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://vuejs.org/" target="_blank">
-            <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-        </a>
+    <div class="container m-auto">
+        <h1 class="text-center my-4">Vibe Check</h1>
+        <h3 class="text-center my-4">Hylé</h3>
+        <div class="flex justify-center">
+            <div class="relative">
+                <video class="" ref="videoFeed" autoplay></video>
+                <canvas class="absolute top-0" ref="canvasOutput"></canvas>
+            </div>
+        </div>
+        <div class="flex justify-center my-8">
+            <button @click="console.log('Vibe Check')">Claim token</button>
+        </div>
     </div>
-    <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+<style>
+h1 {
+    @apply text-4xl;
 }
-.logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-    filter: drop-shadow(0 0 2em #42b883aa);
+
+button {
+    @apply bg-blue-500 text-white font-bold py-2 px-4 rounded;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    build: {
+        sourcemap: 'inline',
+        minify: false,
+    },
     plugins: [vue()],
 });
