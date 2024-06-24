e4acbbac93c831fa42d4cc45ee7a29d039bcf11d
Lancelot de Ferrière <wraitii@gmail.com>
2024-06-24 15:15:07 +0200

    Fetch TX data, will be used to show tokens per user maybe

---
+++ src/LeaderBoard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import Logo from "./assets/Hyle_logo.svg";
+import { GetAllContractRegistrations } from "./indexer";
+
+const contracts = GetAllContractRegistrations();
+
+</script>
+
+<template>
+    <div class="container m-auto">
+        <h1 class="text-center my-4">Vibe Check</h1>
+        <h3 class="text-center my-4"><img :src="Logo" alt="Hylé logo" class="h-10 m-auto"></img></h3>
+
+        <div>
+            <p v-for="contract in contracts" :key="contract.txhash">{{ contract }}</p>
+        </div>
+    </div>
+</template>
+
+<style></style>
