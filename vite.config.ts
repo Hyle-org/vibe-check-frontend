98322e4e06a7d37b2838a9ed3b0ac904f58b97fe
maximilien-hyle <maximilien@hyle.eu>
2024-07-02 14:11:06 +0200

    chore: review comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 import vue from "@vitejs/plugin-vue";
 import copy from "rollup-plugin-copy";
 
@@ -44,6 +45,7 @@ export default defineConfig({
             copySync: true,
             hook: 'buildStart',
         }),
-        wasmContentTypePlugin
+        wasmContentTypePlugin,
+        nodePolyfills(),
     ],
 });
