10f92bacbfe0d79f7f7fd5c794e9cf9e446a045b
Lancelot de Ferrière <wraitii@users.noreply.github.com>
2024-07-04 18:30:50 +0200

    Feat/smaller (#17)
    * Try to improve the package size + Dockerize
    * Fix networks for devnet

---
+++ src/network.ts
@@ -0,0 +1,22 @@
+export const network = import.meta.env.DEV ? "localhost" : "devnet";
+
+export const getNetworkApiUrl = () => {
+    return {
+        localhost: "http://localhost:1317",
+        devnet: "https://api.devnet.hyle.eu",
+    }[network];
+};
+
+export const getNetworkRpcUrl = () => {
+    return {
+        localhost: "http://localhost:26657",
+        devnet: "https://rpc.devnet.hyle.eu",
+    }[network];
+};
+
+export const getNetworkWebsocketUrl = () => {
+    return {
+        localhost: "ws://localhost:26657/websocket",
+        devnet: "wss://rpc.devnet.hyle.eu/websocket",
+    }[network];
+};
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import { nodePolyfills } from "vite-plugin-node-polyfills";
 import vue from "@vitejs/plugin-vue";
 import copy from "rollup-plugin-copy";
 
+import { visualizer } from "rollup-plugin-visualizer";
+import analyze from "rollup-plugin-analyzer";
+
+import topLevelAwait from "vite-plugin-top-level-await";
+
 const wasmContentTypePlugin = {
     name: "wasm-content-type-plugin",
     configureServer(server: any) {
@@ -17,26 +22,13 @@ const wasmContentTypePlugin = {
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    esbuild: {
-        target: "esnext",
-        supported: {
-            "top-level-await": true,
-        },
-    },
-    optimizeDeps: {
-        esbuildOptions: {
-            target: "esnext",
-            supported: {
-                "top-level-await": true,
-            },
-        },
-    },
     build: {
-        sourcemap: "inline",
-        minify: false,
+        sourcemap: false,
+        minify: true,
         target: "esnext",
     },
     plugins: [
+        topLevelAwait(),
         vue(),
         copy({
             targets: [{ src: "node_modules/**/*.wasm", dest: "node_modules/.vite/deps" }],
@@ -44,6 +36,16 @@ export default defineConfig({
             hook: "buildStart",
         }),
         wasmContentTypePlugin,
-        nodePolyfills(),
+        nodePolyfills({
+            include: ["buffer", "path", "fs", "os", "crypto", "stream", "vm"],
+            globals: {
+                Buffer: true, // can also be 'build', 'dev', or false
+                global: true,
+                process: true,
+            },
+            protocolImports: false,
+        }),
+        // visualizer(),
+        // analyze(),
     ],
 });
